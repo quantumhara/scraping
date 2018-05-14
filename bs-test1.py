f84e123c5b9f8d7344f63e16ede1d8da56a2bf92
Schun Uechi <quantum2006hara@gmail.com>
2018-05-14 10:37:50 -0700

    bs-test1.py

--- bs-test1.py
+++ bs-test1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 
 html = """
@@ -5,6 +6,7 @@ html = """
   <h1>スクレイピングとは</h1>
   <p>Webページを解析すること</p>
   <p>任意の箇所を抽出すること</p>
+  <p>やで</p>
 </body></html>
 """
 
@@ -13,7 +15,10 @@ soup = BeautifulSoup(html, 'html.parser')
 h1 = soup.html.body.h1
 p1 = soup.html.body.p
 p2 = p1.next_sibling.next_sibling
+p3 = p2.next_sibling.next_sibling
 
 print("h1 = " + h1.string)
 print("p = " + p1.string)
+print("p = " + p2.string)
+print("p = " + p3.string)
 print("p = " + p2.string)
